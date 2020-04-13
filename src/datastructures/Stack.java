fdbbce9775367c6dff67959dfa060aeddace3f52
= <=>
2020-04-13 22:44:15 +0200

    added Stack implementation with Array

--- src/datastructures/Stack.java
+++ src/datastructures/Stack.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import java.lang.reflect.Array;
+
 public class Stack<T> implements StackOperation<T> {
 
     private Node<T> start;
@@ -31,4 +33,51 @@ interface StackOperation<T> {
     void push(T item);
     T pop();
     boolean isEmpty();
+}
+
+class ArrayStack<T> implements StackOperation<T> {
+
+    private T[] nodes;
+    private final Class<T> _class;
+
+    public ArrayStack(Class<T> _class) {
+        this._class = _class;
+        nodes = createArray(_class, 0);
+    }
+
+    @Override
+    public void push(T item) {
+        increaseArrayByOne();
+        nodes[nodes.length - 1] = item;
+    }
+
+    @Override
+    public T pop() {
+        T tmp = nodes[nodes.length - 1];
+        decreaseArrayByOne();
+        return tmp;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return nodes.length == 0;
+    }
+
+    private void increaseArrayByOne() {
+        T[] tmp = createArray(_class, nodes.length + 1);
+        for (int i = 0; i < nodes.length; i++)
+            tmp[i] = nodes[i];
+        nodes = tmp;
+    }
+
+    private void decreaseArrayByOne() {
+        T[] tmp = createArray(_class, nodes.length - 1);
+        for (int i = 0; i < nodes.length - 1; i++)
+            tmp[i] = nodes[i];
+        nodes = tmp;
+    }
+
+    private T[] createArray(Class<T> t, int length) {
+        return (T[]) Array.newInstance(t, length);
+    }
 }
